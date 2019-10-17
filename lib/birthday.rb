4dd16896ad38a828230132a4b43393f413a1bdb3
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-10-17 14:34:53 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,7 +14,8 @@ end
   birthday_kids.each do |kids_name, age|
     if age <= 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
+    else
+    puts "You are too old for this."
   end
 end
 
